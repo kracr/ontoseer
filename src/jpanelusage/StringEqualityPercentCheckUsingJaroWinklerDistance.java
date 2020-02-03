1294cfe87c3014a2025f8e7ec7a800fe7ae3c0f1
Pramit18142 <42404279+Pramit18142@users.noreply.github.com>
2020-02-03 23:29:04 +0530

    Add files via upload
    ODP interface from terminal

---
+++ src/jpanelusage/StringEqualityPercentCheckUsingJaroWinklerDistance.java
@@ -0,0 +1,102 @@
+package jpanelusage;
+import java.io.*;
+import java.util.*;
+public class StringEqualityPercentCheckUsingJaroWinklerDistance {
+	   public static void main(String args[]) {
+	        printSimilarity("", "");
+	        printSimilarity("", "a");
+	        printSimilarity("aaapppp", "");
+	        printSimilarity("frog", "fog");
+	        printSimilarity("fly", "ant");
+	        printSimilarity("elephant", "hippo");
+	        printSimilarity("hippo", "elephant");
+	        printSimilarity("hippo", "zzzzzzzz");
+	        printSimilarity("hello", "hallo");
+	        printSimilarity("ABC Corporation", "ABC Corp");
+	        printSimilarity("D N H Enterprises Inc", "D & H Enterprises, Inc.");
+	        printSimilarity("My Gym Children's Fitness Center", "My Gym. Childrens Fitness");
+	        printSimilarity("PENNSYLVANIA", "PENNCISYLVNIA");
+	        printSimilarity("The quick fox brown jumped", "The quick brown fox");
+	    }
+	   public static void printSimilarity(String s, String t) {
+	        System.out.println(String.format("%.3f is the similarity between \"%s\" and \"%s\"", similarity(s, t), s, t));
+	    }
+	 
+		/**
+	     * JaroWinklerDistance
+	     * Copied from https://commons.apache.org/sandbox/commons-text/jacoco/org.apache.commons.text.similarity/JaroWinklerDistance.java.html
+	     * apply method changed to similarity
+	     */
+	    public static Double similarity(final CharSequence left, final CharSequence right) {
+	        final double defaultScalingFactor = 0.1;
+	        final double percentageRoundValue = 100.0;
+	 
+	        if (left == null || right == null) {
+	            throw new IllegalArgumentException("Strings must not be null");
+	        }
+	 
+	        int[] mtp = matches(left, right);
+	        double m = mtp[0];
+	        if (m == 0) {
+	            return 0D;
+	        }
+	        double j = ((m / left.length() + m / right.length() + (m - mtp[1]) / m)) / 3;
+	        double jw = j < 0.7D ? j : j + Math.min(defaultScalingFactor, 1D / mtp[3]) * mtp[2] * (1D - j);
+	        return Math.round(jw * percentageRoundValue) / percentageRoundValue;
+	    }
+	    protected static int[] matches(final CharSequence first, final CharSequence second) {
+	        CharSequence max, min;
+	        if (first.length() > second.length()) {
+	            max = first;
+	            min = second;
+	        } else {
+	            max = second;
+	            min = first;
+	        }
+	        int range = Math.max(max.length() / 2 - 1, 0);
+	        int[] matchIndexes = new int[min.length()];
+	        Arrays.fill(matchIndexes, -1);
+	        boolean[] matchFlags = new boolean[max.length()];
+	        int matches = 0;
+	        for (int mi = 0; mi < min.length(); mi++) {
+	            char c1 = min.charAt(mi);
+	            for (int xi = Math.max(mi - range, 0), xn = Math.min(mi + range + 1, max.length()); xi < xn; xi++) {
+	                if (!matchFlags[xi] && c1 == max.charAt(xi)) {
+	                    matchIndexes[mi] = xi;
+	                    matchFlags[xi] = true;
+	                    matches++;
+	                    break;
+	                }
+	            }
+	        }
+	        char[] ms1 = new char[matches];
+	        char[] ms2 = new char[matches];
+	        for (int i = 0, si = 0; i < min.length(); i++) {
+	            if (matchIndexes[i] != -1) {
+	                ms1[si] = min.charAt(i);
+	                si++;
+	            }
+	        }
+	        for (int i = 0, si = 0; i < max.length(); i++) {
+	            if (matchFlags[i]) {
+	                ms2[si] = max.charAt(i);
+	                si++;
+	            }
+	        }
+	        int transpositions = 0;
+	        for (int mi = 0; mi < ms1.length; mi++) {
+	            if (ms1[mi] != ms2[mi]) {
+	                transpositions++;
+	            }
+	        }
+	        int prefix = 0;
+	        for (int mi = 0; mi < min.length(); mi++) {
+	            if (first.charAt(mi) == second.charAt(mi)) {
+	                prefix++;
+	            } else {
+	                break;
+	            }
+	        }
+	        return new int[] { matches, transpositions / 2, prefix, max.length() };
+	    }
+}
---
+++ src/jpanelusage/jpanel.java
@@ -0,0 +1,152 @@
+package jpanelusage;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+public class jpanel extends JFrame  {
+    private JLabel labelEnterdescription = new JLabel("Please enter the description\n" + 
+    		"of Ontology You want to develop ");
+    //private JLabel labeldescriptionanswer = new JLabel("Enter: ");
+    private JLabel labelEnterDomain = new JLabel("Please enter Domain Name you want to\n" + 
+    		"create. ");
+    private JLabel labelelements = new JLabel("Please enter some of the elements like classes and properties you want\n" + 
+    		"to make. ");
+    private JLabel labelcqs = new JLabel("Please enter the competency questions. ");
+    private JLabel labeladditional = new JLabel("Please enter any additional\n" + 
+    		"comments. ");
+    private JLabel finala = new JLabel("Final Input ");
+    
+  //  private JLabel labelPassword = new JLabel("Enter password: ");
+    private JTextField textDescription = new JTextField(20);
+    private JTextField textdomain = new JTextField(20);
+    private JTextField textclass = new JTextField(20);
+    private JTextField textcqs = new JTextField(20);
+    private JTextField textadd = new JTextField(20);
+    private JTextField textfinal = new JTextField(20);
+    //private JPasswordField fieldPassword = new JPasswordField(20);
+    private JButton buttonLogin = new JButton("ODPRecommendation");
+    private JButton close = new JButton("Close the panel");
+    private ActionListener refreshAction = e ->ODP();
+     
+    public jpanel() {
+        super("JPanel Demo Program");
+         
+        // create a new panel with GridBagLayout manager
+        JPanel newPanel = new JPanel(new GridBagLayout());
+         
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.insets = new Insets(10, 10, 10, 10);
+         
+        // add components to the panel
+        constraints.gridx = 0;
+        constraints.gridy = 0;     
+        newPanel.add(labelEnterdescription, constraints);
+        constraints.gridx = 1;
+        newPanel.add(textDescription, constraints);
+        constraints.gridx = 0;
+        constraints.gridy = 1;     
+        newPanel.add( labelEnterDomain, constraints);
+        constraints.gridx = 1;
+        newPanel.add(textdomain, constraints);
+        constraints.gridx = 0;
+        constraints.gridy = 2;     
+        newPanel.add(labelelements, constraints);
+        
+       
+        constraints.gridx = 1;
+        newPanel.add(textclass, constraints);
+     
+        constraints.gridx = 0;
+        constraints.gridy = 3;     
+        newPanel.add(labelcqs, constraints);
+        constraints.gridx = 1;
+        newPanel.add(textcqs, constraints);
+        constraints.gridx = 0;
+        constraints.gridy = 4;     
+        newPanel.add(labeladditional, constraints);
+        constraints.gridx = 1;
+        newPanel.add(textadd, constraints);
+        constraints.gridx = 0;
+        constraints.gridy = 5;     
+        newPanel.add(finala, constraints);
+        constraints.gridx = 1;
+        newPanel.add(textfinal, constraints);
+        textfinal.setEditable(false);
+        /* 
+        constraints.gridx = 0;
+        constraints.gridy = 6;     
+        newPanel.add(labelPassword, constraints);
+         
+        constraints.gridx = 1;
+        newPanel.add(fieldPassword, constraints);
+        */
+         
+        constraints.gridx = 0;
+        constraints.gridy = 7;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        newPanel.add(buttonLogin, constraints);
+        buttonLogin.addActionListener(refreshAction);
+        
+      
+         
+        // set border for the panel
+        newPanel.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), "ODP Recommendation panel"));
+         
+        // add the panel to this frame
+        add(newPanel);
+         
+        pack();
+        setLocationRelativeTo(null);
+    }
+     
+    public void ODP() {
+    	odpdescription o=new odpdescription();
+    	
+    	String s;
+    	String S1=textDescription.getText();
+    	String S2=textdomain.getText();
+    	String S3=textclass.getText();
+    	String S4=textcqs.getText();
+    	String S5=textadd.getText();
+    	s=S1+" "+S2+" "+" "+S3+" "+S4+" "+S5;
+    	//System.out.println(s);
+    	textfinal.setText(s);
+    	o.findsimilarity(s);
+    	//return s;
+    }
+    public void dispose() {
+        
+    	buttonLogin.removeActionListener(refreshAction);
+    	
+
+    }
+    public static void main(String[] args) {
+        // set look and feel to the system look and feel
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+         
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new jpanel().setVisible(true);
+            }
+        });
+    }
+}
+	
